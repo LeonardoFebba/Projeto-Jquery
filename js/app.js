9defc343912a730034f0e752111b372afe452b78
Leo Febba <leonardofebba@hotmail.com>
2021-09-22 19:11:10 -0300

    Adding some style and jquery to the site

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
-// $(window).scroll(function(){
-//     const windowTop = $(this).scrollTop();
-//     $('.anime').each(function(){
-//         if(windowTop > $(this).offset().top - 400){
-//             $(this).addClass('anime-init');
-//         }else{
-//             $(this).removeClass('anime-init');
-//         }
-//     });
-// });
+$('[data-group]').each(function(){
+    const $allTarget = $(this).find('[data-target]'),
+    $allClick = $(this).find('[data-click'),
+    activeClass = 'active';
+    
+    $allTarget.first().addClass(activeClass);
+    $allClick.first().addClass(activeClass);
+
+    $allClick.click(function(e){
+        e.preventDefault();
+        const id = $(this).data('click'),
+        $target = $('[data-target = "'+id+'"');
+        $allClick.removeClass(activeClass);
+        $allTarget.removeClass(activeClass);
+
+        $target.addClass(activeClass);
+        $(this).addClass(activeClass);
+    });
+});
